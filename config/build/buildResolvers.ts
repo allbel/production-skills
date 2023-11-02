c084b0b38c95397c000a9c708941209330ab5cae
Aleksandr Belozerov <allbel900@gmail.com>
2023-11-02 22:21:49 +0300

    implementation FSD (10)

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'], // расширения файлов (при импорте не будем указывать расширения этих файлов)
+    preferAbsolute: true,
+    modules: [options.paths.src, 'node_modules'],
+    mainFiles: ['index'],
+    alias: {}
   }
 }
